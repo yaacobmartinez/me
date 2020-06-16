9147a8131491cfb855b165242cecb5685b3ec92f
Yaacob Martinez <martinezyaacob@gmail.com>
2020-06-16 23:17:43 +0800

    added glitters

--- pages/resume.js
+++ pages/resume.js
@@ -20,7 +20,7 @@ export default function Resume() {
                     <div className={classes.profile}>
                         <Typography variant="h3" style={{ fontFamily: "Poppins", fontWeight: 600, marginBottom: 30 }}>Yaacob <span style={{ color: "#8AB4F8" }}>Martinez</span></Typography>
                         <Typography variant="h5" style={{ fontFamily: "Poppins", fontWeight: 600, color: "#94D1D0", marginBottom: 30 }}>Creative, ambitious, and enterprising web developer. — I strive in teams with strong engineering cultures and meaningful cross-functional collaboration.</Typography>
-                        <Typography variant="h6" style={{ fontFamily: "Poppins", fontWeight: 600, color: "#8AB4F8", marginBottom: 30 }}>martinezyaacob@gmail.com</Typography>
+                        <Typography variant="h6" style={{ fontFamily: "Poppins", fontWeight: 600, color: "#8AB4F8", marginBottom: 30 }}>✉️ martinezyaacob@gmail.com</Typography>
                     </div>
                 </div>
             </Slide>
