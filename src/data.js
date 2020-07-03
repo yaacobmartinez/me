b4ca2f206b23631fe5144595e51d18dbd58de12f
Yaacob Martinez <martinezyaacob@gmail.com>
2020-07-03 14:23:10 +0800

    updated works

--- src/data.js
+++ src/data.js
@@ -1,165 +1,181 @@
 export const links = [
-    { name: "_.Twitter", link: "https://twitter.com/theawkwardjacob" },
-    { name: "_.GitHub", link: "https://github.com/yaacobmartinez" },
-    { name: "_.Facebook", link: "https://www.facebook.com/jacobpogiako" },
-    { name: "_.Mobile: +639159803576", link: "#" },
-]
+	{ name: "_.Twitter", link: "https://twitter.com/theawkwardjacob" },
+	{ name: "_.GitHub", link: "https://github.com/yaacobmartinez" },
+	{ name: "_.Facebook", link: "https://www.facebook.com/jacobpogiako" },
+	{ name: "_.Mobile: +639159803576", link: "#" },
+];
 
 export const experiences = [
-    {
-        position: "IT Lecturer/IT Coordinator",
-        company: "Informatics College",
-        location: "Caloocan",
-        dates: "2018.10 - 2019.10",
-        desc: [
-            ' Plan, evaluate and revise curricula, course content, and course materials and methods of instructions.',
-            'Advise students on academic and vocational curricula and career issues.',
-            'Perform administrative duties such as serving as department head.',
-            'Serve as an academic or administrative committees that deal with institutional policies, departmental matters and academic issues.'
-        ]
-    },
-    {
-        position: "Technical Support Representative",
-        company: "Teleperformance",
-        location: "Pasay",
-        dates: "2017.12 - 2018.09",
-        desc: [
-            'Answer inquiries regarding computer software and hardware operations to resolve problems',
-            'Provide user support by diagnosing network and device problems and implementing technical or procedural solutions.',
-            'Maintain records of daily data communication transactions, problems and remedial actions taken or installation activities.',
-        ]
-    },
-    {
-        position: "Programmer",
-        company: "Bestank Manufacturing Corp.",
-        location: "Valenzuela",
-        dates: "2017.05 - .10",
-        desc: [
-            'Write, update and maintain computer programs or software packages to handle specific jobs such as tracking inventory, storing or retrieving data or controlling other equipment',
-            'Perform or direct revision, repair or expansion of existing programs to increase operating effieiency or adapt to new requirements. ',
-            'Consult with managerial, engineering and technical personnel to clarify program intent, identify problems and suggest changes.',
-        ]
-    },
-    {
-        position: "Web Developer (Freelance)",
-        company: "MBDO Cargo Express Inc.",
-        location: "",
-        dates: "2019.10 - 2020.01",
-        desc: [
-            'Created a Tracking System for Cargo products as well as handle dispatching or orders. ',
-            'Technologies used: NodeJS, Express, Firebase.',
-        ]
-    },
-    {
-        position: "Web Developer (Freelance)",
-        company: "BMWL Medical & Diagnostic Clinic.",
-        location: "",
-        dates: "2019.10 - 2020.01",
-        desc: [
-            'Created a medical record system for staff of the clinic. ',
-            'Technologies used: NodeJS Express, Firebase',
-        ]
-    },
-    {
-        position: "Web Developer (Freelance)",
-        company: "Skin MDC Laser Dermatology Slimming Center",
-        location: "",
-        dates: "2019.10 - 2020.01",
-        desc: [
-            'Created a scheduling system for customers and doctors. ',
-            'Technologies used.: PHP, MySQL',
-        ]
-    },
-    {
-        position: "Web Developer (Freelance)",
-        company: "Informatics College Caloocan",
-        location: "",
-        dates: "2018.01 - 2018.03",
-        desc: [
-            'Created a library management system to be used by the librarian. ',
-            'Technologies used. NodeJS, Firebase',
-        ]
-    },
-]
+	{
+		position: "IT Lecturer/IT Coordinator",
+		company: "Informatics College",
+		location: "Caloocan",
+		dates: "2018.10 - 2019.10",
+		desc: [
+			" Plan, evaluate and revise curricula, course content, and course materials and methods of instructions.",
+			"Advise students on academic and vocational curricula and career issues.",
+			"Perform administrative duties such as serving as department head.",
+			"Serve as an academic or administrative committees that deal with institutional policies, departmental matters and academic issues.",
+		],
+	},
+	{
+		position: "Technical Support Representative",
+		company: "Teleperformance",
+		location: "Pasay",
+		dates: "2017.12 - 2018.09",
+		desc: [
+			"Answer inquiries regarding computer software and hardware operations to resolve problems",
+			"Provide user support by diagnosing network and device problems and implementing technical or procedural solutions.",
+			"Maintain records of daily data communication transactions, problems and remedial actions taken or installation activities.",
+		],
+	},
+	{
+		position: "Programmer",
+		company: "Bestank Manufacturing Corp.",
+		location: "Valenzuela",
+		dates: "2017.05 - .10",
+		desc: [
+			"Write, update and maintain computer programs or software packages to handle specific jobs such as tracking inventory, storing or retrieving data or controlling other equipment",
+			"Perform or direct revision, repair or expansion of existing programs to increase operating effieiency or adapt to new requirements. ",
+			"Consult with managerial, engineering and technical personnel to clarify program intent, identify problems and suggest changes.",
+		],
+	},
+	{
+		position: "Web Developer (Freelance)",
+		company: "MBDO Cargo Express Inc.",
+		location: "",
+		dates: "2019.10 - 2020.01",
+		desc: [
+			"Created a Tracking System for Cargo products as well as handle dispatching or orders. ",
+			"Technologies used: NodeJS, Express, Firebase.",
+		],
+	},
+	{
+		position: "Web Developer (Freelance)",
+		company: "BMWL Medical & Diagnostic Clinic.",
+		location: "",
+		dates: "2019.10 - 2020.01",
+		desc: [
+			"Created a medical record system for staff of the clinic. ",
+			"Technologies used: NodeJS Express, Firebase",
+		],
+	},
+	{
+		position: "Web Developer (Freelance)",
+		company: "Skin MDC Laser Dermatology Slimming Center",
+		location: "",
+		dates: "2019.10 - 2020.01",
+		desc: [
+			"Created a scheduling system for customers and doctors. ",
+			"Technologies used.: PHP, MySQL",
+		],
+	},
+	{
+		position: "Web Developer (Freelance)",
+		company: "Informatics College Caloocan",
+		location: "",
+		dates: "2018.01 - 2018.03",
+		desc: [
+			"Created a library management system to be used by the librarian. ",
+			"Technologies used. NodeJS, Firebase",
+		],
+	},
+];
 
 export const education = [
-    {
-        course: "Bachelor of Science in Information Technology",
-        school: "Informatics College Caloocan",
-        dates: "2014-2017"
-    },
-    {
-        course: "Associate in Computer Technology",
-        school: "Datamex Institute of Computer Technology Caloocan",
-        dates: "2012-2014"
-    },
-    {
-        course: "Bachelor of Science in Business Administration(UnderGrad)",
-        school: "Polytechnic University of the Philippines - Sta. Mesa",
-        dates: "2010-2011"
-    },
-]
+	{
+		course: "Bachelor of Science in Information Technology",
+		school: "Informatics College Caloocan",
+		dates: "2014-2017",
+	},
+	{
+		course: "Associate in Computer Technology",
+		school: "Datamex Institute of Computer Technology Caloocan",
+		dates: "2012-2014",
+	},
+	{
+		course: "Bachelor of Science in Business Administration(UnderGrad)",
+		school: "Polytechnic University of the Philippines - Sta. Mesa",
+		dates: "2010-2011",
+	},
+];
 
 export const works = [
-    {
-        name: "Inventory Plus",
-        shortName: "iplus",
-        img: "/works/iplus.jpg",
-        src: "https://i.imgur.com/cg8GsKi.png",
-        github: "https://github.com/yaacobmartinez/inventoryplus",
-        liveSite: "https://inventory-plus.netlify.app/",
-        desc: "Inventory Plus is an inventory web application I made for my girlfriend's online business ❤️❤️❤️. The app was made in NodeJS as its environment, StrapiCMS as its backend, ReactJS as its frontend library and Material UI as its frontend design.",
-        icon: "/works/iplus-icon.png"
-
-    },
-    {
-        name: "MBDO Tracking",
-        shortName: "mbdo",
-        img: "/works/mbdo.jpg",
-        src: "https://i.imgur.com/LbdlDMJ.png",
-        github: "https://github.com/yaacobmartinez/mbdo.",
-        liveSite: "https://mbdo.vercel.app/",
-        desc: "MBDO Tracking is a tracking web application for all the shipments made by the company.🚚🚚🚚. The app was made in NodeJS as its environment, ExpressJS as its backend, MongoDB as its data store, React and NextJS as its frontend library and Material UI as its frontend design",
-        icon: "/works/mbdo-icon.png"
-    },
-    {
-        name: "Studio Banx",
-        shortName: "banx",
-        img: "/works/banx.jpg",
-        src: "https://i.imgur.com/0X3cdX9.png",
-        github: "https://github.com/yaacobmartinez/studio-banx",
-        liveSite: "https://banx.vercel.app/",
-        desc: "Studio Banx is a landing page I made for a design company. The app was made in NodeJS as its environment, React and NextJS as its frontend library and Material UI as its frontend design",
-        icon: "/works/banx-icon.png"
-    },
-    {
-        name: "Student Council Vote",
-        shortName: "scvote",
-        img: "/works/scvote.jpg",
-        src: "https://i.imgur.com/QcpZxBh.png",
-        github: "https://github.com/yaacobmartinez/SCVote",
-        liveSite: "https://scvote.netlify.app/",
-        desc: "Student Council Vote is a voting web app I made for my former school. The app was made in NodeJS as its environment, ReactJS as its frontend library and Material UI as its frontend design",
-        icon: "/works/scvote-icon.png"
-    },
-    {
-        name: "Messaging App",
-        shortName: "msg",
-        img: "/works/msg.jpg",
-        src: "https://i.imgur.com/hUzMrkF.png",
-        github: "https://github.com/yaacobmartinez/react-messaging",
-        liveSite: "https://react-messaging.netlify.app/",
-        desc: "Messaging is a web app I made which is a mockup of Google Messages. The app was made in NodeJS as its environment, ReactJS as its frontend library and Material UI as its frontend design",
-        icon: "/works/msg-icon.png"
-    },
-    {
-        name: "TaskMaster",
-        shortName: "taskmaster",
-        img: "/works/taskmaster.jpg",
-        src: "https://i.imgur.com/fBsfo1o.png",
-        github: "https://github.com/yaacobmartinez/taskmaster",
-        liveSite: "https://taskmaster.jcbmtz.vercel.app/",
-        desc: "TaskMaster is a web app I made for simply taking down tasks. The app was made in NodeJS as its environment, ReactJS and NextJS as its frontend library and Material UI as its frontend design",
-        icon: "/works/taskmaster-icon.png"
-    },
-]
+	{
+		name: "Inventory Plus",
+		shortName: "iplus",
+		img: "/works/iplus.jpg",
+		src: "https://i.imgur.com/cg8GsKi.png",
+		github: "https://github.com/yaacobmartinez/inventoryplus",
+		liveSite: "https://inventory-plus.netlify.app/",
+		desc:
+			"Inventory Plus is an inventory web application I made for my girlfriend's online business ❤️❤️❤️. The app was made in NodeJS as its environment, StrapiCMS as its backend, ReactJS as its frontend library and Material UI as its frontend design.",
+		icon: "/works/iplus-icon.png",
+	},
+	{
+		name: "MBDO Tracking",
+		shortName: "mbdo",
+		img: "/works/mbdo.jpg",
+		src: "https://i.imgur.com/LbdlDMJ.png",
+		github: "https://github.com/yaacobmartinez/mbdo.",
+		liveSite: "https://mbdo.vercel.app/",
+		desc:
+			"MBDO Tracking is a tracking web application for all the shipments made by the company.🚚🚚🚚. The app was made in NodeJS as its environment, ExpressJS as its backend, MongoDB as its data store, React and NextJS as its frontend library and Material UI as its frontend design",
+		icon: "/works/mbdo-icon.png",
+	},
+	{
+		name: "Studio Banx",
+		shortName: "banx",
+		img: "/works/banx.jpg",
+		src: "https://i.imgur.com/0X3cdX9.png",
+		github: "https://github.com/yaacobmartinez/studio-banx",
+		liveSite: "https://banx.vercel.app/",
+		desc:
+			"Studio Banx is a landing page I made for a design company. The app was made in NodeJS as its environment, React and NextJS as its frontend library and Material UI as its frontend design",
+		icon: "/works/banx-icon.png",
+	},
+	{
+		name: "Datamex Saint Adeline Website",
+		shortName: "datamex",
+		img: "https://i.imgur.com/Nbv0Ghdt.jpg",
+		src: "https://i.imgur.com/Nbv0Ghd.jpg",
+		github: "https://github.com/yaacobmartinez/datamex",
+		liveSite: "https://datamex.vercel.app/",
+		desc:
+			"Datamex Saint Adeline is a website I made for my previous school. The website was made in NodeJS as its environment, React and NextJS as its frontend library and Material UI as its frontend design",
+		icon: "https://i.imgur.com/SgNtGejt.png",
+	},
+	{
+		name: "Student Council Vote",
+		shortName: "scvote",
+		img: "/works/scvote.jpg",
+		src: "https://i.imgur.com/QcpZxBh.png",
+		github: "https://github.com/yaacobmartinez/SCVote",
+		liveSite: "https://scvote.netlify.app/",
+		desc:
+			"Student Council Vote is a voting web app I made for my former school. The app was made in NodeJS as its environment, ReactJS as its frontend library and Material UI as its frontend design",
+		icon: "/works/scvote-icon.png",
+	},
+	{
+		name: "Messaging App",
+		shortName: "msg",
+		img: "/works/msg.jpg",
+		src: "https://i.imgur.com/hUzMrkF.png",
+		github: "https://github.com/yaacobmartinez/react-messaging",
+		liveSite: "https://react-messaging.netlify.app/",
+		desc:
+			"Messaging is a web app I made which is a mockup of Google Messages. The app was made in NodeJS as its environment, ReactJS as its frontend library and Material UI as its frontend design",
+		icon: "/works/msg-icon.png",
+	},
+	{
+		name: "TaskMaster",
+		shortName: "taskmaster",
+		img: "/works/taskmaster.jpg",
+		src: "https://i.imgur.com/fBsfo1o.png",
+		github: "https://github.com/yaacobmartinez/taskmaster",
+		liveSite: "https://taskmaster.jcbmtz.vercel.app/",
+		desc:
+			"TaskMaster is a web app I made for simply taking down tasks. The app was made in NodeJS as its environment, ReactJS and NextJS as its frontend library and Material UI as its frontend design",
+		icon: "/works/taskmaster-icon.png",
+	},
+];
--- src/theme.js
+++ src/theme.js
@@ -5,18 +5,6 @@ import { red } from '@material-ui/core/colors';
 const theme = createMuiTheme({
     palette: {
         type: 'dark',
-        // primary: {
-        //     main: '#556cd6',
-        // },
-        // secondary: {
-        //     main: '#19857b',
-        // },
-        // error: {
-        //     main: red.A400,
-        // },
-        // background: {
-        //     default: '#fff',
-        // },
     },
 });
 
